30675e8a087b8376a45a7b8c3860f16da26a1912
inspiration2026 <inspiration.thailand@gmail.com>
2026-02-03 15:05:05 +0800

    Add alghorithm of adding new book to array Add validation to form Add check for doubles after adding a new book Add Event Listeners for all buttons

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const myLibrary = [];
 
-function Book(title,author,pages) {
+function Book(title,author,pages,read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read ? "YES" : "NO";
     this.id = crypto.randomUUID();
   // the constructor...
 }
 
 
-function addBookToLibrary(title,author,pages) {
-    let book = new Book (title,author,pages);
+function addBookToLibrary(title,author,pages,read) {
+    let book = new Book (title,author,pages,read);
+    console.table(book)
+    console.table(title,author,pages,read)
     myLibrary.push (book);
+
+    let isDuplicate = false;
+
+    for (let i = 0; i < (myLibrary.length - 1); i++) {
+        let existingBook = myLibrary[i];
+
+        if (existingBook.title === book.title &&
+            existingBook.author === book.author &&
+            existingBook.pages === book.pages) {
+            isDuplicate = true;
+            break;           
+        }
+    };
+        if (isDuplicate === true) {
+        myLibrary.pop();
+        console.log("Duplicate found → removed");
+        } else {
+        console.log("Book added successfully");
+    }
 }
 
 function createTable() {
@@ -20,16 +42,18 @@ function createTable() {
     const table = document.createElement("table");
     table.classList.add ("table_books");
     const caption = document.createElement("caption");
-    caption.textContent = ("Books in the Library")
+    caption.textContent = ("Books in the Library");
     const title_row = document.createElement("tr");
     const head1 = document.createElement("th");
     const head2 = document.createElement("th");
     const head3 = document.createElement("th");
     const head4 = document.createElement("th");
-    head1.textContent = ("Title")
-    head2.textContent = ("Author")
-    head3.textContent = ("Pages")
-    head4.textContent = ("ID #")
+    const head5 = document.createElement("th");
+    head1.textContent = ("Title");
+    head2.textContent = ("Author");
+    head3.textContent = ("Pages");
+    head4.textContent = ("ID #");
+    head5.textContent = ("Read already");
 
     document.body.appendChild(div);
     div.appendChild(table);
@@ -39,26 +63,33 @@ function createTable() {
     title_row.appendChild(head2);
     title_row.appendChild(head3);
     title_row.appendChild(head4);
+    title_row.appendChild(head5);
+
+    div.style.height = "50vh";
+    div.style.overflowY = "auto";
 }
 
 function displayBook(book) {
-    const row = document.createElement("tr")
+    const row = document.createElement("tr");
     const data1 = document.createElement("td");
     const data2 = document.createElement("td");
     const data3 = document.createElement("td");
     const data4 = document.createElement("td");
+    const data5 = document.createElement("td");
     const table = document.querySelector("table");
 
-    data1.textContent = (book.title)
-    data2.textContent = (book.author)
-    data3.textContent = (book.pages)
-    data4.textContent = (book.id)
+    data1.textContent = (book.title);
+    data2.textContent = (book.author);
+    data3.textContent = (book.pages);
+    data4.textContent = (book.id);
+    data5.textContent = (book.read);
 
     table.appendChild (row);
     row.appendChild (data1);
     row.appendChild (data2);
     row.appendChild (data3);
     row.appendChild (data4);
+    row.appendChild (data5);
 }
 
 addBookToLibrary("Fourth Wing", "Rebecca Yarros", "517");
@@ -94,10 +125,9 @@ button.textContent = ("New Book");
 // Creating request form 
     const form_container = document.createElement ("div");
     Object.assign(form_container.style, {
-        display: "flex",
+        display: "none",
         justifyContent: "center",
-        marginTop: "20px",
-        border: "2px solid purple"
+        marginTop: "20px"
     });
     const bookForm = document.createElement ("form");
     document.body.appendChild(form_container);
@@ -108,10 +138,8 @@ button.textContent = ("New Book");
         gridTeamplateColumn: "1fr 2fr",
         columnGap: "10px",
         rowGap: "10px",
-        // flexDirection: "column",
-        // justifyContent: "center",
-        // marginTop: "20px",
-        border: "2px solid red"
+        padding: "10px",
+        border: "1px solid grey",
     });
 
     const labelOne = document.createElement ("label");
@@ -174,8 +202,8 @@ button.textContent = ("New Book");
     bookForm.appendChild(readLabel);
     bookForm.appendChild(readCheckbox);
 
-    const submit = document.createElement ("button")
-    const cancel = document.createElement ("button")
+    const submit = document.createElement ("button");
+    const cancel = document.createElement ("button");
 
     submit.type = "submit";
     submit.textContent = "Add Book";
@@ -188,15 +216,40 @@ button.textContent = ("New Book");
     Object.assign(submit.style, {
         width: "100px",
         height: "30px",
-        backgroundColor: "#e2e1e1"
+        backgroundColor: "#e2e1e1",
+        border: "1px solid #736f6f"
     });
 
     Object.assign(cancel.style, {
         width: "100px",
         height: "30px",
-        backgroundColor: "#e2e1e1"
+        backgroundColor: "#e2e1e1",
+        border: "1px solid #736f6f"
     });
 
+// Event Listeners
+
+button.addEventListener ("click", function() {
+    form_container.style.display = "flex"
+});
+
+cancel.addEventListener ("click", function() {
+    form_container.style.display = "none"
+});
+
+submit.addEventListener ("click", function(event) {
+    event.preventDefault();
+
+    const oldArrayLenght = myLibrary.length;
+    addBookToLibrary (inputOne.value, inputTwo.value, inputThree.value, readCheckbox.checked);
+
+    if (myLibrary.length>oldArrayLenght) {
+        const last = myLibrary[(myLibrary.length - 1)];
+        displayBook(last);
+        style();
+    }
+    
+});
 
 
 
@@ -211,12 +264,14 @@ button.textContent = ("New Book");
 
 
 // Styling (Should be in the end of JS file)
+function style() {
 const table = document.querySelector("table");
 table.style.borderCollapse = "collapse";
 table.style.border = "1px solid #ccc";
 table.style.width = "90%";
 table.style.margin = "20px auto";
 table.style.fontFamily = "Arial, sans-serif";
+
 
 const caption = document.querySelector ("caption");
 caption.style.padding = "12px 15px";
@@ -225,12 +280,13 @@ caption.style.padding = "12px 15px";
 document.querySelectorAll("th").forEach (th => {
     th.style.padding = "12px 15px";
     th.style.border = "1px solid #ccc";
-})
+});
 document.querySelectorAll("td").forEach (td => {
     td.style.padding = "12px 15px";
     td.style.border = "1px solid #ccc";
-})
+});
 document.querySelectorAll("tr").forEach (tr => {
     tr.style.borderBottom = "3px solid #ddd";
-})
-
+});
+}
+style();
